ddb806092a28a71c24a45264f3b4f9c52bd30a21
raptor72 <mctaras51@yandex.ru>
2019-11-12 22:48:00 +0400

    correct config name, add now() func in logging

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,11 +2,12 @@
 
 import os
 import re
-from collections import namedtuple, OrderedDict
+from collections import namedtuple
 import gzip
 import sys
 import json
 import logging
+from datetime import datetime
 
 logging.basicConfig(filename="log_analyzer.log", level=logging.INFO)
 
@@ -15,16 +16,19 @@ logging.basicConfig(filename="log_analyzer.log", level=logging.INFO)
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 
-config = {
+default_config = {
     "REPORT_SIZE": 1,
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log"
 }
 
+def now():
+    return str(datetime.now())
 
 def get_external_config():
-    external_config = sys.argv[1]
-    if external_config:
+    config = default_config
+    try:
+        external_config = sys.argv[1]
         if os.path.exists(external_config):
             with open(external_config, 'r') as conf:
                 external_settings = json.load(conf)
@@ -35,6 +39,8 @@ def get_external_config():
         else:
             logging.info("use default config")
         logging.info(f"resule config is {config}")
+    except IndexError:
+        pass
     return config
 
 def get_last_log(logdir):
@@ -47,7 +53,8 @@ def get_last_log(logdir):
             if file > last:
                 last = file
                 date = file.split("-")[-1].split(".")[0]
-                path = str(os.path.abspath(config["LOG_DIR"])) + "/"  + last
+#                path = str(os.path.abspath(config["LOG_DIR"])) + "/"  + last
+                path = str(os.path.abspath(logdir)) + "/" + last
                 extension = "gz" if last.split(".")[-1] == "gz" else ""
     logging.info(f"choised log file is {last}")
     logging.info("no log files matched")
@@ -126,7 +133,7 @@ def mediana(data):
         result = smass[mid]
     return result
 
-def handle_dict(d, all_time):
+def handle_dict(d, all_time, report_size=default_config["REPORT_SIZE"]):
     res = []
     other = []
 #    replacement = {direct_count, time_avg, time_max, time_sum, all_count, url}
@@ -144,7 +151,7 @@ def handle_dict(d, all_time):
         pay.append(time_perc)
         d[i] = pay
     for i, j in d.items():
-        if j[3] < float(config["REPORT_SIZE"]):
+        if j[3] < float(report_size):
             continue
         else:
             res.append( [i, *j])
@@ -156,9 +163,9 @@ def handle_dict(d, all_time):
 
 
 def main():
-    logging.info("script started at")
+    logging.info("script started at " + now())
     config = get_external_config()
-
+    print(config)
     my_log = get_last_log(config["LOG_DIR"])
     reportname = "report" + my_log.date + ".html"
     reportfile = str(os.path.abspath(config["REPORT_DIR"])) + "/"  + reportname
@@ -176,7 +183,7 @@ def main():
             print(dic)
 #            pass
         all_time = dic[1]
-        d1 = handle_dict(dic[0], dic[1])
+        d1 = handle_dict(dic[0], dic[1], config["REPORT_SIZE"])
 
         with open("report.html", "r") as report:
             data = report.read()
@@ -186,7 +193,7 @@ def main():
         with open(reportfile, "w") as report:
             report.write(data)
 
-    logging.info("script done at")
+    logging.info("script done at " + now())
 
 if __name__ == "__main__":
     main()
